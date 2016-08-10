429ac555fe3a325f8ce6fc6d8e391524d3074164
Gupta <avagupta@paypalcorp.com>
2016-08-10 18:27:34 +0530

    Added Rummy.java

---
+++ Rummy.java
@@ -0,0 +1,15 @@
+package structures;
+
+public class Rummy {
+	
+	int num_of_decks = 2;
+	int num_of_jokers_per_deck = 2;
+	Deck D = new Deck(num_of_decks,num_of_jokers_per_deck);
+	
+	
+	
+	
+	
+	
+
+}
