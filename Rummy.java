47f0c242fb6251065424cb0dde8f6a6b82abd4f4
Pushpinder Singh <pushpisingh@paypal.com>
2016-08-11 09:06:24 +0530

    Added deck and hand in Rummy

--- Rummy.java
+++ Rummy.java
@@ -1,21 +1,19 @@
 package structures;
 
 public class Rummy {
-	 
-	Rummy()
-	{
-		
-	int num_of_decks = 2;
-	int num_of_jokers_per_deck = 2;
-	int numOfCardsInHand = 13;
-	Deck D = new Deck(num_of_decks,num_of_jokers_per_deck);
-	D.shuffle();
-	Card newJoker = D.draw();
-	
-	Hand hand = new hand(D.draw(numOfCardsInHand));
+
+	private Hand hand;
+
+	public Rummy() {
+
+		Deck d = new Deck(2, 2);
+		Hand h = new Hand();
+		for (int i = 0; i < 13; i++) {
+			d.shuffle();
+			h.addCard(d.draw());
+		}
+
+		this.hand = h;
 	}
-	
-	
-	
-	
-    }
+
+}
