5dae20f8b9abdc2fa055db8381043b3109e63262
Pushpinder Singh <pushpisingh@paypal.com>
2016-08-11 10:27:26 +0530

    Added more test data

--- TestRummy.java
+++ TestRummy.java
@@ -26,12 +26,24 @@ public class TestRummy {
 		Rummy game2 = new Rummy(mixed);
 		game2.evaluate();
 
+		ArrayList<Card> seqHand = new ArrayList<>();
+		seqHand.addAll(getNofSameFace(1, 4));
+		seqHand.addAll(getNofSameFace(1, 5));
+		seqHand.addAll(getNofSameFace(1, 6));
+		seqHand.addAll(getNofSameFace(1, 7));
+		seqHand.addAll(getNofSameFace(2, 8));
+		seqHand.addAll(getNofSameFace(1, 9));
+		seqHand.addAll(getNofSameFace(1, 10));
+		Hand seq = new Hand(seqHand);
+		Rummy game3 = new Rummy(seq);
+		game3.evaluate();
+
 	}
 
 	public static ArrayList<Card> getNofSameFace(int n, int face) {
 		ArrayList<Card> allSameFaces = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
-			allSameFaces.add(new Card(2, face));
+			allSameFaces.add(new Card(i % 4, face));
 		}
 
 		return allSameFaces;
