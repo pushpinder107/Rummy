c8820cce06b65f9f20303312127be7efababac15
Pushpinder Singh <pushpisingh@paypal.com>
2016-08-12 08:58:07 +0530

    Integrated InputParser

--- TestRow.java
+++ TestRow.java
@@ -5,65 +5,63 @@ import java.util.ArrayList;
 public class TestRow {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-		String input = "SDf";//pushp fn
-		for(int i=1;i<input.length();i++)
-		{	
-			ArrayList<Row> rows =new ArrayList<Row>();//input function
+		String input = "SDf";// pushp fn
+		boolean keepRunning = true;
+		while (keepRunning) {
+			ArrayList<Row> rows = new ArrayList<Row>();
+			Row inputRow = InputParser.getInputRow();
+			if (inputRow == null) {
+				break;
+			}
+			rows.add(inputRow);// input function
 			Row r = rows.get(0);
-			ArrayList<Row> ans = new ArrayList<Row>() ;
+			ArrayList<Row> ans = new ArrayList<Row>();
 			ans.add(r);
-			for(int j=1;j<rows.size()-1;j++)
-			{	
-				ArrayList<Row> ans2 = new ArrayList<Row>() ;
+			for (int j = 1; j < rows.size() - 1; j++) {
+				ArrayList<Row> ans2 = new ArrayList<Row>();
 				Row newRow = rows.get(j);
-				for(int k = 0; k< ans.size(); k++)
-				{
+				for (int k = 0; k < ans.size(); k++) {
 					int cases = newRow.getCase(ans.get(k));
-					//Cases are for subset suoersett same etc
-					if(cases == 1){
+					// Cases are for subset suoersett same etc
+					if (cases == 1) {
 						ans2.add(newRow);
 						ans2.add(ans.get(k));
 					}
-					if(cases == 2){
+					if (cases == 2) {
 						ans2.add(ans.get(k));
 						ans2.add(newRow);
 					}
-					if(cases == 3){
-						Row r1 = new Row(ans.get(k).start,newRow.start-1,ans.get(k).track,ans.get(k).code);						
-						//Row r2 = new Row(ans.get(k).start,newRow.start-1,ans.get(k).track,ans.get(k).code);
-						Row r3 = new Row(newRow.end+1,ans.get(k).end,ans.get(k).track,ans.get(k).code);
+					if (cases == 3) {
+						Row r1 = new Row(ans.get(k).start, newRow.start - 1, ans.get(k).track, ans.get(k).code);
+						Row r3 = new Row(newRow.end + 1, ans.get(k).end, ans.get(k).track, ans.get(k).code);
 						ans2.add(r1);
 						ans2.add(newRow);
 						ans2.add(r3);
 					}
-					if(cases == 4){
-						Row r3 = new Row(newRow.end+1,ans.get(k).end,ans.get(k).track,ans.get(k).code);
+					if (cases == 4) {
+						Row r3 = new Row(newRow.end + 1, ans.get(k).end, ans.get(k).track, ans.get(k).code);
 						ans2.add(newRow);
 						ans2.add(r3);
 					}
-					if(cases == 5){
-						Row r1 = new Row(ans.get(k).start,newRow.start-1,ans.get(k).track,ans.get(k).code);
-						ans2.addAll(r3);
-						ans2.add(newRow);						
-						
+					if (cases == 5) {
+						Row r1 = new Row(ans.get(k).start, newRow.start - 1, ans.get(k).track, ans.get(k).code);
+						// ans2.addAll(r3);
+						ans2.add(newRow);
+
 					}
-					if(cases == 6)//exact same or newRow is superSet
+					if (cases == 6)// exact same or newRow is superSet
 					{
 						ans2.add(newRow);
 					}
-					
-					
+
 				}
-				ans = ans2;		
-						
+				ans = ans2;
+
 			}
 			// call merge function on ans2
 		}
 		// call merge function on ans2
-		//return ans2
-=======
+		// return ans2
 		while (true) {
 			Row r = InputParser.getInputRow();
 			if (r == null) {
@@ -73,7 +71,6 @@ public class TestRow {
 
 			System.out.println(r.toString());
 		}
->>>>>>> 92ca9a8c307955f98251a7f4779bac31fa4450d1
 	}
 
 }
