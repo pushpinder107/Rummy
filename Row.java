ef573e08b78c0e8bb97dcd0fac400a300d395a5f
Kumar <harshkumar@paypalcorp.com>
2016-08-12 08:39:42 +0530

    sdf

--- Row.java
+++ Row.java
@@ -1,21 +1,40 @@
 package tracking;
 
 public class Row {
-
-	private int start;
-	private int end;
-	private char trackingId;
-	private int transferCode;
-
-	public Row(int s, int e, char t, int tc) {
-		this.start = s;
-		this.end = e;
-		this.trackingId = t;
-		this.transferCode = tc;
+	
+	
+	int start;
+	int end;
+	char track;
+	int code;
+	
+	public Row(int start2, int i, char track2, int code2) {
+		// TODO Auto-generated constructor stub
+		this.start = start2;
+		this.end = i;
+		this.track = track2;
+		this.code = code2;
+	}
+	
+	public String toString()
+	{
+		return this.start+" "+this.end+ " "+ this.track + " " + this.code;
+		
 	}
 
-	public String toString() {
-		String str = "Start : " + start + " End : " + end + " Tracking : " + trackingId + " Transfer : " + transferCode;
-		return str;
+	public int getCase(Row first) {
+		if (this.end < first.start)
+			return 1;
+		else if (this.start > first.end)
+			return 2;
+		else if (this.start > first.start && this.end < first.end)
+			return 3;
+		else if (this.start < first.start && this.end < first.end)
+			return 4;
+		else if (this.start > first.start && this.end > first.end)
+			return 5;
+		else if ((this.start <= first.start && this.end >= first.end))
+			return 6;
+		return 0;
 	}
 }
--- TestRow2.java
+++ TestRow2.java
@@ -2,21 +2,29 @@ package tracking;
 
 import java.util.ArrayList;
 
-public class TestRow {
-
+public class TestRow2 {
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String input = "SDf";//pushp fn
-		for(int i=1;i<input.length();i++)
+		
+		Row r11 = new Row(1,10,'A',3);
+		Row r22 = new Row(8,14,'A',3);
+		ArrayList<Row> input =new ArrayList<Row>();//input function
+		input.add(r11);
+		input.add(r22);
+		ArrayList<Row> ans2;//check this
+		for(int i=1;i<input.size();i++)
 		{	
-			ArrayList<Row> rows =new ArrayList<Row>();//input function
-			Row r = rows.get(0);
+			System.out.println("sdf:");
+			Row r = input.get(0);
 			ArrayList<Row> ans = new ArrayList<Row>() ;
 			ans.add(r);
-			for(int j=1;j<rows.size()-1;j++)
+			for(int j=1;j<input.size()-1;j++)
 			{	
-				ArrayList<Row> ans2 = new ArrayList<Row>() ;
-				Row newRow = rows.get(j);
+				ans2 = new ArrayList<Row>() ;
+				Row newRow = input.get(j);
 				for(int k = 0; k< ans.size(); k++)
 				{
 					int cases = newRow.getCase(ans.get(k));
@@ -25,6 +33,7 @@ public class TestRow {
 						ans2.add(newRow);
 						ans2.add(ans.get(k));
 					}
+					
 					if(cases == 2){
 						ans2.add(ans.get(k));
 						ans2.add(newRow);
@@ -44,7 +53,7 @@ public class TestRow {
 					}
 					if(cases == 5){
 						Row r1 = new Row(ans.get(k).start,newRow.start-1,ans.get(k).track,ans.get(k).code);
-						ans2.addAll(r3);
+						ans2.add(r1);
 						ans2.add(newRow);						
 						
 					}
@@ -53,7 +62,7 @@ public class TestRow {
 						ans2.add(newRow);
 					}
 					
-					
+					System.out.println(newRow.toString());
 				}
 				ans = ans2;		
 						
@@ -61,7 +70,8 @@ public class TestRow {
 			// call merge function on ans2
 		}
 		// call merge function on ans2
-		//return ans2
+		//return ans2;
+		//System.out.println(ans2.get(0).toString());
 	}
 
 }
